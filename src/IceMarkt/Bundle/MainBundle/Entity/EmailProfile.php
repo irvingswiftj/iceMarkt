7daaa6058f85082bb76c5d7f6c72d90e0b2c9eaa
James Irving-Swift <james@irving-swift.com>
2014-08-10 20:09:25 +0200

    created two new entities. EmailProfile and EmailTemplate

---
+++ src/IceMarkt/Bundle/MainBundle/Entity/EmailProfile.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: james
+ * Date: 10/08/2014
+ * Time: 19:55
+ */
+
+namespace IceMarkt\Bundle\MainBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="email_profile")
+ */
+class EmailProfile
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $fromEmail;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $fromName;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $replyToEmail;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $replyToName;
+
+    /**
+     * @ORM\OneToMany(targetEntity="EmailTemplate", mappedBy="EmailProfile")
+     */
+    private $templates;
+
+    public function __construct()
+    {
+        $this->templates = new ArrayCollection();
+    }
+
+    /**
+     * @param String $fromEmail
+     */
+    public function setFromEmail($fromEmail)
+    {
+        $this->fromEmail = $fromEmail;
+    }
+
+    /**
+     * @return String
+     */
+    public function getFromEmail()
+    {
+        return $this->fromEmail;
+    }
+
+    /**
+     * @param String $fromName
+     * @return $this;
+     */
+    public function setFromName($fromName)
+    {
+        $this->fromName = $fromName;
+
+        return $this;
+    }
+
+    /**
+     * @return String
+     */
+    public function getFromName()
+    {
+        return $this->fromName;
+    }
+
+    /**
+     * @return Integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param String $replyToEmail
+     * @return $this;
+     */
+    public function setReplyToEmail($replyToEmail)
+    {
+        $this->replyToEmail = $replyToEmail;
+
+        return $this;
+    }
+
+    /**
+     * @return String
+     */
+    public function getReplyToEmail()
+    {
+        return $this->replyToEmail;
+    }
+
+    /**
+     * @param String $replyToName
+     * @return $this
+     */
+    public function setReplyToName($replyToName)
+    {
+        $this->replyToName = $replyToName;
+
+        return $this;
+    }
+
+    /**
+     * @return String
+     */
+    public function getReplyToName()
+    {
+        return $this->replyToName;
+    }
+
+
+}
---
+++ src/IceMarkt/Bundle/MainBundle/Entity/EmailTemplate.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: james
+ * Date: 10/08/2014
+ * Time: 19:47
+ */
+
+namespace IceMarkt\Bundle\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="email_templates")
+ */
+class EmailTemplate
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $subject;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $template;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $format;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="EmailProfile", inversedBy="EmailTemplate")
+     * @ORM\JoinColumn(name="email_profile_id", referencedColumnName="id")
+     */
+    private $profile;
+
+    /**
+     * @param mixed $format
+     * @return $this;
+     */
+    public function setFormat($format)
+    {
+        $this->format = $format;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+    /**
+     * @param mixed $id
+     * @return $this;
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $name
+     * @return $this;
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $subject
+     * @return $this;
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param mixed $template
+     * @return $this;
+     */
+    public function setTemplate($template)
+    {
+        $this->template = $template;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTemplate()
+    {
+        return $this->template;
+    }
+}
