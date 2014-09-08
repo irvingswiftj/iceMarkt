4d2ef628546e505a91137fcb9b1bf9c96a714f51
James Irving-Swift <james@irving-swift.com>
2014-09-08 16:57:32 +0200

    Added missing files Removed nfs drive mapping

--- puphpet/shell/important-notices.sh
+++ puphpet/shell/important-notices.sh
@@ -3,9 +3,9 @@
 VAGRANT_CORE_FOLDER=$(cat '/.puphpet-stuff/vagrant-core-folder.txt')
 
 if [[ -f '/.puphpet-stuff/displayed-important-notices' ]]; then
-    return 0
+    exit 0
 fi
 
-cat "${VAGRANT_CORE_FOLDER}/shell/important-notices.txt"
+cat "${VAGRANT_CORE_FOLDER}/shell/ascii-art/important-notices.txt"
 
 touch '/.puphpet-stuff/displayed-important-notices'
